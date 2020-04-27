5934837d0ae84df13021525fc9e706f817950751
unknown <kavunv@gmail.com>
2020-04-27 20:14:51 +0300

    Updates

---
+++ precache-manifest.5e5cc608118d816d055697e15d7a811a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c11569213011ff0c3ba9e4e6e68b56af",
+    "url": "/calculator/index.html"
+  },
+  {
+    "revision": "03c9346e8f7abc9f68f3",
+    "url": "/calculator/static/css/main.499e78d4.chunk.css"
+  },
+  {
+    "revision": "88b7f6336c186f14011f",
+    "url": "/calculator/static/js/2.2706ab41.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/calculator/static/js/2.2706ab41.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "03c9346e8f7abc9f68f3",
+    "url": "/calculator/static/js/main.f6544fbb.chunk.js"
+  },
+  {
+    "revision": "552c96952084d63edc80",
+    "url": "/calculator/static/js/runtime-main.29434a71.js"
+  }
+]);
